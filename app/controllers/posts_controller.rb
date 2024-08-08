eab26823a8717da0f7714a4bd10a732ed925e88d
tonniegeetz <tonniegeetz@gmail.com>
2024-08-08 05:38:16 +0300

    controller validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
-    render json: post
+    render json: post, status: :accepted
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -19,5 +20,4 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w[fiction non-fiction] }
+  validates :content, length: { minimum: 100 }
 end
